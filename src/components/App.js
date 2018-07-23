210d6ed0fef6b9d0136fb343e53bac781c2e0299
Daria Kalinina <lynxx91@mail.ru>
2018-07-23 18:32:45 +0300

    initial commit + change test in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import articles from '../fixtures'
 function App() {
     return (
         <div>
-            <h1>App name</h1>
+            <h1>App name!</h1>
             <Article article={articles[0]} foo="bar" flag />
         </div>
     )
